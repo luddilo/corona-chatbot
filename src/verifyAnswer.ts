53bd28817a1ed996f3f0fb8fab41d325d26b9a88
luddilo <ludvig@furhatrobotics.com>
2020-04-04 15:16:12 +0200

    Updated script to take the "Skip verification" col into account, and added strings to verifyAnswer

--- src/verifyAnswer.ts
+++ src/verifyAnswer.ts
@@ -2,40 +2,40 @@ import { BotTurn, ANYTHING } from "narratory"
 import { confirmWrong, confirmRight } from "./nlu"
 const config = require("../../config.json")
 
-export const verifyAnswer : BotTurn = {
-    label: "VERIFY_ANSWER",
-    say: {
-      text: "Förstod jag din fråga korrekt?",
-      suggestions: ["Ja", "Nej"]
-    },
-    user: [
-      { intent: confirmRight, bot: "Tack!" },
-      {
-        intent: confirmWrong,
-        bot: {
-          say: [
-            "Oj. Jag förstår. Tack för hjälpen att bli bättre!",
-            "Oj. Sorry. Tack för att du hjälper mig bli bättre!"
-          ],
-          url: config.confirmWrongAnswerWebhook,
-          params: ["classifiedIntentName", "classifiedUtterance", "botResponse"],
-          asyncWebhook: true
-        }
+export const verifyAnswer: BotTurn = {
+  label: "VERIFY_ANSWER",
+  say: {
+    text: "Förstod jag din fråga korrekt?",
+    suggestions: ["Ja", "Nej"],
+  },
+  user: [
+    { intent: confirmRight, bot: ["Okej. Tack ska du ha!", "Härligt. Tack för hjälpen!"] },
+    {
+      intent: confirmWrong,
+      bot: {
+        say: [
+          "Okej. Jag förstår. Tack för hjälpen att bli bättre!",
+          "Okej. Sorry. Tack för att du hjälper mig bli bättre!",
+        ],
+        url: config.confirmWrongAnswerWebhook,
+        params: ["classifiedIntentName", "classifiedUtterance", "botResponse"],
+        asyncWebhook: true,
       },
-      {
-        intent: ANYTHING,
-        bot: [
-          {
-            cond: {
-              retryCount: 0
-            },
-            say: "Förlåt, var det rätt eller fel svar?",
-            repair: true
+    },
+    {
+      intent: ANYTHING,
+      bot: [
+        {
+          cond: {
+            retryCount: 0,
           },
-          {
-            say: "Jag förstod inte, men låt oss gå vidare"
-          }
-        ]
-      }
-    ]
-  }
+          say: "Förlåt, var det rätt eller fel svar?",
+          repair: true,
+        },
+        {
+          say: "Jag förstod inte, men låt oss gå vidare",
+        },
+      ],
+    },
+  ],
+}

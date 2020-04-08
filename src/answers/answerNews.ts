4b0e18345a4ac5e38aaa949c2996b5314191c13a
Kajsa Tretow <kajsa@Kajsas-Air.lan>
2020-04-08 12:44:11 +0200

    implemented nested responses

--- src/answers/answerNews.ts
+++ src/answers/answerNews.ts
@@ -1,24 +1,33 @@
-import { BotTurn, Card } from "narratory"
+import { BotTurn, Card, BridgeTurn, Image } from "narratory"
 
 const card = new Card({
-    title: "Statistik 7 april",
+    title: "Statistik",
     description: "",
-  })
+    image: new Image("https://files.slack.com/files-pri/TVCDNNBNG-F011BSZM0GK/statistik_fhm_7april.png", "some alt"),
+})
 
-export const answerNews: BotTurn = {
-    say: [
-        {
+export const answerNews: Array<BotTurn | BridgeTurn> = [
+    {
         cond: {
-          platform: "voximplant"
+            platform: "voximplant"
         },
-        text:
-          "Okej, här är senaste nytt från Folkhälsomyndighetens dagliga presskonferens från tisdagen den 7 april. Globalt är 1,3 miljoner människor bekräftat smittade och 74000 döda, med en minskad andel från Europa då smittan har börjat sakta ner i Italien och Spanien, samtidigt som den ökar kraftigt i USA. I Sverige har vi ungefär 7700 bekräftade fall av covid19, varav 640 har vårdats i på intensivvård, och vi är nu uppe i 591 rapporterade dödsfall. Folkhälsomyndighetens Anders Tegnell sa att är viktigt att vi försöker undvika sociala sammanhang under den kommande påskhelgen och påpekade att de regler som gäller på verksamheter som barer och caféer för att undvika trängsel även gäller på uteserveringar."
-      },
-      {
-        text:
-          "Okej, här är senaste nytt från Folkhälsomyndighetens dagliga presskonferens från tisdagen den 7 april. Folkhälsomyndighetens Anders Tegnell redovisade aktuella siffror (se nedan) och nämnde att vi globalt ser en minskad andel från Europa då smittan har börjat sakta ner i Italien och Spanien, samtidigt som den ökar kraftigt i USA. Här i Sverige är det viktigt att vi försöker undvika sociala sammanhang under den kommande påskhelgen och Tegnell påpekade även att de regler som gäller på verksamheter som barer och caféer för att undvika trängsel även gäller på uteserveringar",
-        content: card
-      }
-    ],
-    goto: "VERIFY_ANSWER"
-  }
+        say: "Okej, här är senaste nytt från Folkhälsomyndighetens dagliga presskonferens från tisdagen den 7 april. Globalt är 1,3 miljoner människor bekräftat smittade och 74000 döda, men en minskad andel kommer från Europa då smittan har börjat sakta ner i Italien och Spanien, samtidigt som den ökar kraftigt i USA. I Sverige har vi ungefär 7700 bekräftade fall av covid19, varav 640 har intensivvårdats, och vi är nu uppe i 591 rapporterade dödsfall. Enligt stadsepidemilog Anders Tegnell  är det viktigt att vi försöker undvika sociala sammanhang under den kommande påskhelgen och han påpekade särskilt att de regler som gäller på verksamheter som barer och caféer för att undvika trängsel även gäller på uteserveringar.",
+        goto: "VERIFY_ANSWER"
+    },
+    {
+        cond: {
+            platform: "voximplant"
+        },
+        say: "Okej, här är senaste nytt från Folkhälsomyndighetens dagliga presskonferens från tisdagen den 7 april. Globalt är 1,3 miljoner människor bekräftat smittade och 74000 döda, men en minskad andel kommer från Europa då smittan har börjat sakta ner i Italien och Spanien, samtidigt som den ökar kraftigt i USA.",
+        bot: {
+            say: {
+                text: "I Sverige har vi ungefär 7700 bekräftade fall av covid19, varav 640 har intensivvårdats, och vi är nu uppe i 591 rapporterade dödsfall.",
+                content: card
+            },
+            bot: {
+                say: "Enligt stadsepidemilog Anders Tegnell  är det viktigt att vi försöker undvika sociala sammanhang under den kommande påskhelgen och han påpekade särskilt att de regler som gäller på verksamheter som barer och caféer för att undvika trängsel även gäller på uteserveringar.",
+                goto: "VERIFY_ANSWER"
+            }
+        }
+    }
+]

83b27bae62946cc45aff4f64e2fcc84a56a8f0bb
Kajsa Tretow <kajsa@Kajsas-Air.lan>
2020-04-11 13:02:42 +0200

    update wording intensive care

--- src/answers/answerIntensiveCare.ts
+++ src/answers/answerIntensiveCare.ts
@@ -5,15 +5,15 @@ export const answerIntensiveCare: Array<BridgeTurn | BotTurn> = [
         cond: {
             region: true
         }, 
-        say: "Tyvärr har jag ingen regional data från intensivvården, men i hela Sverige har vi sammanlagt haft _intensive_care patienter i intensivvård till följd av covid19.",
+        say: "I _region har sammanlagt _intensive_care patiender fått intensivvård till följd av covid19.",
     },
     {
         cond: {
             day: "idag"
         },
-        say: "Jag kan tyvärr inte svara hur många som intensivvårdas just nu, men i Sverige har vi sammanlagt haft _intensive_care patienter i intensivvård till följd av covid19"
+        say: "Jag kan tyvärr inte svara hur många som får intensivvård just nu, men i Sverige har vi sammanlagt haft _intensive_care patienter i intensivvård till följd av covid19"
     },
     {
-        say: "I Sverige har vi sammanlagt haft _intensive_care patienter i intensivvård till följd av covid19."
+        say: "I Sverige har sammanlagt _intensive_care patienter fått intensivvård till följd av covid19."
     }
 ]
